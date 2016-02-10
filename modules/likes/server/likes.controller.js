38dc2f062155f2eab6ebea51e1700b297659e0c2
Peter Andersen <peter@ryanandersen.dk>
2016-02-10 12:34:23 -0500

    added likes api

---
+++ modules/likes/server/likes.controller.js
@@ -0,0 +1,90 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var dynamoose = require('dynamoose');
+var Likes = dynamoose.model('Likes');
+var errorHandler = require('modules/core/server/errors.controller');
+var _ = require('lodash');
+
+/**
+ * Show
+ */
+exports.read = function (req, res) {
+  res.json(req.model);
+};
+
+/**
+ * Create
+ */
+exports.create = function (req, res) {
+  var item = new Likes(req.body);
+
+  item.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(item);
+    }
+  });
+};
+
+/**
+ * Delete
+ */
+exports.delete = function (req, res) {
+  var item = req.model;
+
+  item.delete().then(function(result){
+    res.json(item);
+  })
+  .catch(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+  });
+};
+
+/**
+ * List
+ */
+exports.list = function (req, res) {
+  Likes.scan().exec().then(function(items){
+    res.json(items);
+  })
+  .catch(function(err){
+    return res.status(400).send({
+      message: errorHandler.getErrorMessage(err)
+    });
+  });
+};
+
+/**
+ * middleware
+ */
+exports.findByID = function (req, res, next, id) {
+  if (!_.isString(id)) {
+    return res.status(400).send({
+      message: 'Likes is invalid'
+    });
+  }
+
+  Likes.get(id).then(function(item){
+    if(_.isUndefined(item)){
+      return res.status(400).send({
+        message: 'Likes not found'
+      });
+    }
+
+    req.model = item;
+    next();
+  })
+  .catch(function(err){
+    return next(err);
+  });
+};
