b744e58b79445d6879601e1091a04f0b992daf62
Peter Andersen <peter@ryanandersen.dk>
2016-02-16 21:30:34 -0500

    IN-661 #comment added first version of channel, message and member rest api

---
+++ modules/member/server/member.controller.js
@@ -0,0 +1,113 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var dynamoose = require('dynamoose');
+/* global -Promise */
+var Promise = require('bluebird');
+var moment = require('moment');
+var redis = require('config/lib/redis');
+var Member = dynamoose.model('Member');
+var errorHandler = require('modules/core/server/errors.controller');
+var _ = require('lodash');
+
+/**
+ * Show
+ */
+exports.read = function (req, res) {
+  res.json(req.model);
+};
+
+/**
+ * Create
+ */
+exports.create = function (req, res) {
+  var item = new Member(req.body);
+
+  item.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(item);
+    }
+  });
+};
+
+/**
+ * Update
+ */
+exports.update = function (req, res) {
+  var item = req.model;
+
+  //For security purposes only merge these parameters
+  item.body = req.body.body;
+
+  item.save().then(function(data){
+    res.json(item);
+  })
+  .catch(function (err) {
+    return res.status(400).send({
+      message: errorHandler.getErrorMessage(err)
+    });
+  });
+};
+
+/**
+ * Delete
+ */
+exports.delete = function (req, res) {
+  var item = req.model;
+
+  item.delete().then(function(result){
+    res.json(item);
+  })
+  .catch(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    }
+  });
+};
+
+/**
+ * List
+ */
+exports.list = function (req, res) {
+  Member.scan().exec().then(function(items){
+    res.json(items);
+  })
+  .catch(function(err){
+    return res.status(400).send({
+      message: errorHandler.getErrorMessage(err)
+    });
+  });
+};
+
+/**
+ * middleware
+ */
+exports.findByID = function (req, res, next, id) {
+  if (!_.isString(id)) {
+    return res.status(400).send({
+      message: 'Member is invalid'
+    });
+  }
+
+  Member.get(id).then(function(item){
+    if(_.isUndefined(item)){
+      return res.status(400).send({
+        message: 'Member not found'
+      });
+    }
+
+    req.model = item;
+    next();
+  })
+  .catch(function(err){
+    return next(err);
+  });
+};
