62ace2132dc19d0e09586c9c97b88f066dc60fcc
Thomas O'Brien <tbobrien612@gmail.com>
2016-02-17 16:35:35 -0500

    IN-646 #comment Send to user and send to all company members added to sendgrid service.

--- modules/sendgrid/server/sendgrid.service.js
+++ modules/sendgrid/server/sendgrid.service.js
@@ -6,7 +6,11 @@
 var config = require('config/config');
 /* global -Promise */
 var Promise = require('bluebird');
+var dynamoose = require('dynamoose');
 var sendgrid = Promise.promisifyAll(require('sendgrid')(config.sendgrid.username, config.sendgrid.pass));
+var Team = dynamoose.model('Team');
+var User = dynamoose.model('User');
+var _this = this;
 
 /**
  * Service for all sendgrid emails
@@ -38,28 +42,50 @@ exports.send = Promise.method(function(to, subject, substitutions, template) {
 });
 
 /**
- * Service for sending sendgrid emails to all members of a company
+ * Send to a user
  */
-// exports.sendToCompanyUsers = function(emailObj, companyId, template) {
-//   if (emailObj) {
-//     emailObj.setFilters({
-//       'templates': {
-//         'settings': {
-//           'enable': 1,
-//           'template_id': template
-//         }
-//       }
-//     });
+exports.sendToUser = function(userId, subject, substitutions, template) {
+  return User.get(userId)
+    .then(function(user) {
+      return _this.send(user.email, subject, substitutions, template);
+    })
+    .catch(function(error) {
+      console.log('error: ', error);
+      throw error;
+    });
+};
 
-//     if (config.sendgrid && config.sendgrid.local) {
-//       console.log('We didn\'t send an email. Here\'s the url we would\'ve put in it: ', url);
-//       console.log('Here\'s the template we would\'ve used: ', template);
-//       if (callback) {
-//         callback();
-//       }
-//     } else {
-//       sendgrid.send(emailObj, function(err, json) {
-//       });
+/**
+ * Send to all members of a company
+ */
+exports.sendToCompanyUsers = function(emailObj, subject, substitutions, template, companyId) {
+  return Team.query({ companyId: companyId }).exec().then(function(items) {
+    return Promise.map(items, function(item) {
+      _this.sendToUser(item.userId);
+    });
+  });
+};
+
+// var email = new sendgrid.Email();
+// email.addTo(to);
+// email.subject = subject;
+// email.setSubstitutions(substitutions);
+// email.from = 'mailer@sproutup.co';
+// email.fromname = 'SproutUp';
+// email.html = '<div></div>';
+// email.setFilters({
+//   'templates': {
+//     'settings': {
+//       'enable': 1,
+//       'template_id': template
 //     }
 //   }
-// };
+// });
+
+// if (config.sendgrid && config.sendgrid.local) {
+//   console.log('We didn\'t send an email. Here are the sendgrid substitutions: ', substitutions);
+//   console.log('Here\'s the template we would\'ve used: ', template);
+//   return 'OK';
+// } else {
+//   return sendgrid.sendAsync(email);
+// }
