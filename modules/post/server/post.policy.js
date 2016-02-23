77aab2c1c6e6c9cf3ef557264ef2fd610eeff6dd
Peter Andersen <peter@ryanandersen.dk>
2016-02-22 23:39:48 -0500

    bugfix: add buzz access too logged in user

--- modules/post/server/post.policy.js
+++ modules/post/server/post.policy.js
@@ -29,6 +29,15 @@ exports.invokeRolesPolicies = function () {
     }, {
       resources: '/api/post/:postId',
       permissions: ['*']
+    }, {
+      resources: '/api/post/timeline/all',
+      permissions: ['*']
+    }, {
+      resources: '/api/post/timeline/all/:index',
+      permissions: ['*']
+    }, {
+      resources: '/api/post/timeline/group/:groupId/:index',
+      permissions: ['*']
     }]
   }, {
     roles: ['guest'],
