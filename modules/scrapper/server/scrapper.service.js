5da9ea632797fab1c400ccc9d0a772d9137ec7e1
Thomas O'Brien <tbobrien612@gmail.com>
2016-03-04 16:55:53 -0500

    SU-729 #comment Scrapper service, controller, and api created

---
+++ modules/scrapper/server/scrapper.service.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+/* global -Promise */
+var Promise = require('bluebird');
+var rp = require('request-promise');
+var extractor = require('unfluff');
+
+/**
+ * Scrape html from a url and return unfluff'd data object 
+ */
+exports.getMeta = Promise.method(function(url) {
+  return rp(url)
+    .then(function(body) {
+      var data = extractor.lazy(body);
+      return data;
+    })
+    .catch(function(err) {
+      return false; 
+    });
+
+    // You can take the returned data object to access whichever data elements you need directly.
+    // console.log(data.title());
+    // console.log(data.date());
+    // console.log(data.author());
+    // console.log(data.text());
+    // console.log(data.image());
+    // console.log(data.tags());
+    // console.log(data.videos());
+    // ...
+});
