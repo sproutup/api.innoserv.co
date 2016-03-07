2e93e7e94c8ef53ec923e4dff8f10040047185ea
Peter Andersen <peter@ryanandersen.dk>
2016-03-07 11:07:32 -0500

    Suggestion model and rest api. first working version

---
+++ modules/suggestion/server/suggestion.model.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var dynamoose = require('dynamoose');
+var Schema = dynamoose.Schema;
+var FlakeId = require('flake-idgen');
+var flakeIdGen = new FlakeId();
+var intformat = require('biguint-format');
+var validator = require('validator');
+
+/**
+ * Schema
+ */
+var SuggestionSchema = new Schema({
+  id: {
+    type: String,
+    default: function(){ return intformat(flakeIdGen.next(), 'dec'); },
+    hashKey: true
+  },
+  userId: {
+    type: String,
+    required: true,
+    index: {
+      global: true,
+      rangeKey: 'created',
+      project: true, // ProjectionType: ALL
+      throughput: 2 // read and write are both 5
+    }
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  url: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  name: {
+    type: String,
+    default: '',
+    trim: true,
+    required: true
+  }
+});
+
+/**
+ * Populate method
+ */
+SuggestionSchema.method('populate', function (_schema, _id) {
+  var _this = this;
+  var _attribute = _schema.toLowerCase() + 'Id';
+  console.log('populate: ', _schema);
+  var model = dynamoose.model(_schema);
+  return model.get(this[_attribute]).then(function(item){
+    _this[_schema.toLowerCase().trim()] = item;
+    return _this;
+  });
+});
+
+dynamoose.model('Suggestion', SuggestionSchema);
+
