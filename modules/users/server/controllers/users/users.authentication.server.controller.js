66b09e86116f95a6a3f96701e4b36695533e9c56
Peter Andersen <peter@ryanandersen.dk>
2016-04-01 17:07:03 -0400

    fixed the https issue

--- modules/users/server/controllers/users/users.authentication.server.controller.js
+++ modules/users/server/controllers/users/users.authentication.server.controller.js
@@ -263,7 +263,12 @@ exports.oauthCall = function (strategy, scope) {
     console.log('strategy: ', strategy, scope);
     console.log('x-forwarded-proto: ', req.headers['x-forwarded-proto']);
     console.log('referer: ', req.headers.referer);
-    req.headers['x-forwarded-proto'] = 'https';
+
+    var tls = (req.headers.referer && 'https' === req.headers.referer.split(/\s*,\s*/)[0]);
+    var protocol = tls ? 'https' : 'http';
+    console.log('proto: ', protocol);
+    req.headers['x-forwarded-proto'] = protocol;
+
     passport.authenticate(strategy, scope)(req, res, next);
   };
 };
@@ -280,6 +285,7 @@ exports.oauthCallback = function (strategy) {
 
     passport.authenticate(strategy, function (err, user, redirectURL) {
       if (err) {
+        console.log('err: ', err);
         return res.redirect('/authentication/signin?err=' + encodeURIComponent(errorHandler.getErrorMessage(err)));
       }
       if (!user) {
