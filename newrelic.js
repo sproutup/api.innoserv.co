e2d27983272f8d7ec858f2886cbe4489b896c450
Peter Andersen <peter@ryanandersen.dk>
2016-06-24 12:15:11 -0400

    added new relic module and config

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['sproutup'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '804e8f49f033cb0cc754faa2f571126525ff4fa0',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
